c8932bcc90b04ee3edfa62fff59c617bb0376d30
Ujjawal <ujjwl0143@gmail.com>
2024-09-02 00:04:53 +0530

    Second commit

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,29 +1,60 @@
-import 'package:dating_app/screens/home_screen.dart';
-import 'package:dating_app/screens/loginSignup_screen.dart';
-import 'package:dating_app/screens/profile_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:dating_app/screens/profile_screen.dart';
+
 class HomeScreen extends StatefulWidget {
   @override
   _HomeScreenPageState createState() => _HomeScreenPageState();
 }
+
 class _HomeScreenPageState extends State<HomeScreen> {
   bool _isPersonIconSelected = false;
   bool _isWhatshotIconSelected = true;
   bool _isMessageIconSelected = false;
 
+  var images = [
+    'images/girl.webp',
+    'images/girl2.webp',
+    'images/girl3.webp',
+    'images/girl4.webp',
+    'images/girl5.webp',
+  ];
+
+  var names = [
+    'Emilia Clarke',
+    'Sophie Turner',
+    'Maisie Williams',
+    'Arya Stark',
+    'Sansa Stark',
+  ];
+
+  var universities = [
+    'Lovely Professional University',
+    'Harvard University',
+    'Stanford University',
+    'Massachusetts Institute of Technology',
+    'University of California',
+  ];
+
+  var locations = [
+    'Punjab',
+    'New York',
+    'California',
+    'Boston',
+    'Los Angeles',
+  ];
+
   @override
   Widget build(BuildContext context) {
-
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      // Removes the debug banner
+      debugShowCheckedModeBanner: false, // Removes the debug banner
       home: Scaffold(
         appBar: AppBar(
           title: Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
               IconButton(
-                icon: Icon(Icons.person, color: _isPersonIconSelected ? Colors.pink : Colors.grey),
+                icon: Icon(Icons.person,
+                    color: _isPersonIconSelected ? Colors.pink : Colors.grey),
                 onPressed: () {
                   setState(() {
                     _isPersonIconSelected = true;
@@ -37,13 +68,13 @@ class _HomeScreenPageState extends State<HomeScreen> {
                 },
               ),
               IconButton(
-                icon: Icon(Icons.whatshot, color: _isWhatshotIconSelected ? Colors.pink : Colors.grey),
+                icon: Icon(Icons.whatshot,
+                    color: _isWhatshotIconSelected ? Colors.pink : Colors.grey),
                 onPressed: () {
                   setState(() {
                     _isPersonIconSelected = false;
                     _isWhatshotIconSelected = true;
                     _isMessageIconSelected = false;
-
                   });
                   Navigator.push(
                     context,
@@ -52,13 +83,15 @@ class _HomeScreenPageState extends State<HomeScreen> {
                 },
               ),
               IconButton(
-                icon: Icon(Icons.message, color: _isMessageIconSelected ? Colors.pink : Colors.grey),
+                icon: Icon(Icons.message,
+                    color: _isMessageIconSelected ? Colors.pink : Colors.grey),
                 onPressed: () {
                   setState(() {
                     _isPersonIconSelected = false;
                     _isWhatshotIconSelected = false;
                     _isMessageIconSelected = true;
                   });
+                  // Uncomment below to navigate to the message screen
                   // Navigator.push(
                   //   context,
                   //   MaterialPageRoute(builder: (context) => MessagePage()),
@@ -74,65 +107,88 @@ class _HomeScreenPageState extends State<HomeScreen> {
           children: [
             // Image with rounded corners and overlays
             Expanded(
-              child: Stack(
-                children: [
-                  Container(
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(20.0),
-                      image: DecorationImage(
-                        image: AssetImage('images/girl.webp'), // Replace with your image asset
-                        fit: BoxFit.cover,
-                      ),
-                    ),
-                    margin: EdgeInsets.all(16.0),
-                  ),
-                  Positioned(
-                    bottom: 20.0,
-                    left: 20.0,
-                    right: 20.0,
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          'Emilia Clarke',
-                          style: TextStyle(
-                            color: Colors.black,
-                            fontSize: 24.0,
-                            fontWeight: FontWeight.bold,
+              child: PageView.builder(
+                itemCount: images.length,
+                itemBuilder: (context, index) {
+                  return Stack(
+                    children: [
+                      Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20.0),
+                          image: DecorationImage(
+                            image: AssetImage(images[index]),
+                            fit: BoxFit.cover,
                           ),
                         ),
-                        SizedBox(height: 8.0),
-                        Row(
+                        margin: EdgeInsets.all(16.0),
+                      ),
+                      Positioned(
+                        bottom: 20.0,
+                        left: 20.0,
+                        right: 20.0,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
-                            Icon(Icons.school, color: Colors.black),
-                            SizedBox(width: 8.0),
                             Text(
-                              'Lovely Professional University',
+                              names[index],
                               style: TextStyle(
                                 color: Colors.black,
-                                fontSize: 18.0,
+                                fontSize: 24.0,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            SizedBox(height: 8.0),
+                            Row(
+                              children: [
+                                Icon(Icons.school, color: Colors.black),
+                                SizedBox(width: 8.0),
+                                Text(
+                                  universities[index],
+                                  style: TextStyle(
+                                    color: Colors.black,
+                                    fontSize: 18.0,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            SizedBox(height: 8.0),
+                            Row(
+                              children: [
+                                Icon(Icons.location_on, color: Colors.black),
+                                SizedBox(width: 8.0),
+                                Text(
+                                  locations[index],
+                                  style: TextStyle(
+                                    color: Colors.black,
+                                    fontSize: 18.0,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            SizedBox(height: 16.0),
+                            GestureDetector(
+                              onTap: () {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => ProfilePage()),
+                                );
+                              },
+                              child: Text(
+                                'Meet ${names[index]}',
+                                style: TextStyle(
+                                  color: Colors.blue,
+                                  fontSize: 18.0,
+                                  decoration: TextDecoration.underline,
+                                ),
                               ),
                             ),
                           ],
                         ),
-                        SizedBox(height: 8.0),
-                        Row(
-                          children: [
-                            Icon(Icons.location_on, color: Colors.black),
-                            SizedBox(width: 8.0),
-                            Text(
-                              'Punjab',
-                              style: TextStyle(
-                                color: Colors.black,
-                                fontSize: 18.0,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
+                      ),
+                    ],
+                  );
+                },
               ),
             ),
             // Round-shaped boxes with icons and shadow
